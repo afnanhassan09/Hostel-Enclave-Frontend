0b78f73b191a73744c1e96dfb338e1414524bc48
Afnan Hassan <hassanafnan09@gmail.com>
2024-07-25 20:24:35 +0500

    Renamed and changed the icon

--- Helpers/menu.jsx
+++ Helpers/menu.jsx
@@ -1,25 +1,39 @@
-// components/MenuBar.js
-'use client'
-import Link from  'react'
-
+'use client';
+import { useState } from 'react';
+import Link from 'next/link';
 
 const MenuBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="bg-black p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-xl font-bold" >
-            <Link href="/"> Hostel Enclave</Link>
-  
+        <div className="text-white text-xl font-bold">
+          <Link href="/">Hostel Enclave</Link>
         </div>
-        <div className="flex space-x-4 ">
-          <Link href="/hostels" className="text-gray-300 hover:text-white">
-          See Hostels
+        {/* Mobile menu button */}
+        <button
+          className="text-white block lg:hidden"
+          onClick={toggleMenu}
+        >
+          ☰
+        </button>
+        {/* Menu links */}
+        <div
+          className={`lg:flex lg:items-center lg:space-x-4 ${isOpen ? 'block' : 'hidden'} absolute lg:relative top-16 left-0 w-full lg:w-auto bg-black lg:bg-transparent`}
+        >
+          <Link href="/hostels" className="text-gray-300 hover:text-white block lg:inline-block p-2">
+            See Hostels
           </Link>
-          <Link className="text-gray-300 hover:text-white" href="/about">
-          About
+          <Link href="/about" className="text-gray-300 hover:text-white block lg:inline-block p-2">
+            About
           </Link>
-          <Link href="/contact" className="text-gray-300 hover:text-white">
-           Contact
+          <Link href="/contact" className="text-gray-300 hover:text-white block lg:inline-block p-2">
+            Contact
           </Link>
         </div>
       </div>
