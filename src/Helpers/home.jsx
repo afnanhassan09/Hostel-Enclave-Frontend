43fc43a88eff13375a00e38f756e2e1c54df541e
Afnan Hassan <hassanafnan09@gmail.com>
2024-07-26 11:00:14 +0500

    Corrected the hostels and about us

--- src/Helpers/home.jsx
+++ src/Helpers/home.jsx
@@ -9,16 +9,16 @@ export default function HomeO() {
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="relative flex items-center justify-center h-full text-center">
         <div className="text-white">
-          <h1 className="text-4xl md:text-6xl font-bold mb-4 no-underline">Welcome to Islamabad Hostel Community</h1>
-          <p className="text-lg md:text-2xl mb-8 no-underline">Book a hostel </p>
-        
-            <button className="px-6 py-3 bg-transparent hover:bg-black text-white font-semibold rounded-md no-underline" >
+          <h1 className="text-4xl md:text-6xl font-bold mb-4 no-underline">Welcome to Hostel Enclave</h1>
+          <p className="text-lg md:text-2xl mb-8 no-underline">Find the best hostels</p>
+
+          <button className="px-6 py-3 text-black bg-white hover:bg-black hover:text-white font-semibold rounded-md no-underline" >
             <Link to="/hostels">
-            Hostels
+              Hostels
             </Link>
-           
-            </button>
-          
+
+          </button>
+
         </div>
       </div>
     </div>
